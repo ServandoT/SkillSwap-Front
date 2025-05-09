c6effbb0757eff5753e89c5b08af20efabee51e0
ServandoT <gonzalez.agar.carlos@iescamas.es>
2025-05-09 13:40:07 +0200

    estilos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,19 +19,21 @@ function About() {
         <section className='primera-seccion'>
           <img src="/cerebro.jpg" alt="cerebro" />
 
-          <div className='p-wrapper'>
+          <div className='wrapper'>
             <p>SkillSwap es un espacio donde puedes aprender nuevas habilidades y enseñar lo que sabes sin necesidad de dinero. A través de un sistema de créditos, puedes impartir clases y ganar créditos para inscribirte en otras.</p>
           </div>
         </section>
 
         <section className='segunda-seccion'>
           <h2>¿Cómo funciona?</h2>
-          <ol>
-            <li>Regístrate y recibe créditos iniciales.</li>
-            <li>Elige una clase que te interese y úsalo para inscribirte.</li>
-            <li>Comparte tu conocimiento ofreciendo clases y gana más créditos.</li>
-            <li>Aprende sin límites e intercambia habilidades con otros usuarios.</li>
-          </ol>
+          <div className="wrapper">
+            <ol>
+              <li>Regístrate y recibe créditos iniciales.</li>
+              <li>Elige una clase que te interese y úsalo para inscribirte.</li>
+              <li>Comparte tu conocimiento ofreciendo clases y gana más créditos.</li>
+              <li>Aprende sin límites e intercambia habilidades con otros usuarios.</li>
+            </ol>
+          </div>
           <img src="networking.png" alt="networking" />
         </section>
 
