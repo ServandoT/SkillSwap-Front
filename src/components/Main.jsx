e00e9973b2810f8e0a9edf36f2f77642badfb0d5
ServandoT <gonzalez.agar.carlos@iescamas.es>
2025-05-20 20:25:21 +0200

    Filtros de búsqueda

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,71 +1,3 @@
-// import React, { use, useEffect, useState } from 'react';
-// import axios from 'axios';
-// import Clase from './Clase';
-// import '../styles/Main.css';
-
-// function Main() {
-//   const URL_API = import.meta.env.VITE_URL_API;
-//   const [clases, setClases] = useState([]);
-//   const [categorias, setCategorias] = useState([]);
-
-//   useEffect(() => {
-//     axios.get(`${URL_API}/clases`, {
-//       headers: {
-//         Authorization: '',
-//       }
-//     })
-//       .then((res) => {
-//         setClases(res.data);
-//       })
-//       .catch((error) => {
-//         console.error('Error cargando las clases:', error);
-//       });
-//   }, []);
-
-//   useEffect(() => {
-//     axios.get(`${URL_API}/categorias`, {
-//       headers: {
-//         Authorization: '',
-//       }
-//     })
-//       .then((res) => {
-//         setCategorias(res.data);
-//       })
-//       .catch((error) => {
-//         console.error('Error cargando las categorias:', error);
-//       });
-//   }, []);
-
-//   return (
-//     <main>
-
-//       <div className="cabecera">
-//         <h1>Nuestras Clases</h1>
-//         <div className="filtros">
-//           <form>
-//             <select>
-//               <option value="">Selecciona una categoria</option>
-//               {categorias.map((categoria) => (
-//                 <option key={categoria.id} value={categoria.id}>
-//                   {categoria.nombre}
-//                 </option>
-//               ))}
-//             </select>
-//           </form>
-//         </div>
-//       </div>
-
-//       <div className="clases">
-//         {clases.map(clase => (
-//           <Clase clase={clase} />
-//         ))}
-//       </div>
-//     </main>
-//   );
-// }
-
-// export default Main;
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Clase from './Clase';
@@ -76,9 +8,14 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 function Main() {
   const URL_API = import.meta.env.VITE_URL_API;
   const [clases, setClases] = useState([]);
+  const [clasesFiltradas, setClasesFiltradas ] = useState([]);
   const [categorias, setCategorias] = useState([]);
   const [itemOffset, setItemOffset] = useState(0);
   const itemsPerPage = 9;
+  // TODO filtros
+  const [ filtroNombre, setFiltroNombre ] = useState('');
+  const [ filtroCategoria, setFiltroCategoria ] = useState('');
+
 
   useEffect(() => {
     axios.get(`${URL_API}/clases`,
@@ -90,6 +27,7 @@ function Main() {
     )
       .then((res) => {
         setClases(res.data);
+        setClasesFiltradas(res.data);
       })
       .catch((error) => {
         console.error('Error cargando las clases:', error);
@@ -114,13 +52,37 @@ function Main() {
 
   // Calcular clases para mostrar en la página actual
   const endOffset = itemOffset + itemsPerPage;
-  const currentItems = clases.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(clases.length / itemsPerPage);
+  const currentItems = clasesFiltradas.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(clasesFiltradas.length / itemsPerPage);
 
-  const handlePageClick = (event) => {
+  const handlePageClick = (event) => {  
     const newOffset = (event.selected * itemsPerPage) % clases.length;
     setItemOffset(newOffset);
   };
+
+  useEffect(() => {
+    if (filtroNombre) {
+      const clasesFiltradas = clases.filter(clase => 
+        clase.titulo.toLowerCase().includes(filtroNombre.toLowerCase())
+      );
+      setClasesFiltradas(clasesFiltradas);
+    } else {
+      setClasesFiltradas(clases);
+    }
+  }, [filtroNombre]);
+
+  useEffect(() => {
+    if (filtroCategoria) {
+      const clasesFiltradas = clases.filter(clase => 
+        clase.categorias.some(categoria =>
+          categoria.nombre.toLowerCase() === filtroCategoria.toLocaleLowerCase()
+        )
+      );
+      setClasesFiltradas(clasesFiltradas);
+    } else {
+      setClasesFiltradas(clases);
+    }
+  }, [filtroCategoria]);
 
   return (
     <main>
@@ -128,14 +90,15 @@ function Main() {
         <h1>Nuestras Clases</h1>
         <div className="filtros">
           <form>
-            <select>
+            <select onChange={ (e) => setFiltroCategoria(e.target.value) }>
               <option value="">Selecciona una categoria</option>
               {categorias.map((categoria) => (
-                <option key={categoria.id} value={categoria.id}>
+                <option key={categoria.id} value={categoria.nombre}>
                   {categoria.nombre}
                 </option>
               ))}
             </select>
+            <input type="text" onChange={ (e) => setFiltroNombre(e.target.value) }/>
           </form>
         </div>
       </div>
